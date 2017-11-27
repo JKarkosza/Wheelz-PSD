6a831dcf3eead97d3f88c1481ecb529bc62bc8f8
JKarkosza <jakubkarkosza@gmail.com>
2017-11-27 10:26:37 +0100

    google maps api

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function initMap() {
+  var cracow = {lat: 50.064820, lng: 19.942387};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: cracow,
+    fullscreenControl: false,
+    streetViewControl: false
+  });
+  var marker = new google.maps.Marker({
+    position: cracow,
+    map: map
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+
+console.log("DOM fully loaded");
+
+});
